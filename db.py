ad8cd574572963e3ad6ee0ec2e54125cea4a3d78
nausheen04huri-commits <nausheen04huri@gmail.com>
2026-09-13 15:07:19 +0530

    Add port parameter to database connection
    Updated get_db_connection to include port configuration.

--- db.py
+++ db.py
@@ -3,8 +3,9 @@ import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        user=os.getenv("DB_USER", "root"),
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", "3306")),
+        user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME", "beyondmarks")
+        database=os.getenv("DB_NAME")
     )
